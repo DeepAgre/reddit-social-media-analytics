bf194007f1d0b11857acebf494dffb9ffc886763
DeepAgre <deepagre1234@gmail.com>
2025-10-17 13:54:00 +0530

    reddit_credentials.py

--- reddit_credentials.py
+++ reddit_credentials.py
@@ -1,3 +1,4 @@
-client_id = "u0jkdlb6tCkMS3X6OEQtHg"
-client_secret = "BVdSEu7V-nbuNUhQqx8PRr_LDE2DDA"
-user_agent = "TechScraper/1.0"
+client_id = "your id here"
+client_secret = "your secret id here"
+
+user_agent = "ur agent name here"
